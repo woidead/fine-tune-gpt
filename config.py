22a03fb8e76ed41d1782c041ec8227139639c445
aziz <abikbest05@gmail.com>
2024-01-16 04:10:23 +0600

    added config

---
+++ config.py
@@ -0,0 +1,4 @@
+prompt = "A model doing something blah blah blah"
+temperature = .4 #the level of creativity is from 0 to 1. where 1 is very creative, and closer to 0 for working with exact values
+number_of_examples = 100 #how many example datas you would create
+GPT_KEY = "YOUR_KEY"
